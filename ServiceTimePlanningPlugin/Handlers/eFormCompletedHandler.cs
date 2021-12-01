09b2fad92ef82f04a020d82cc58a783d03311110
René Schultz Madsen <rm@microting.com>
2021-12-01 08:06:56 +0000

    Adding more log messages.

--- ServiceTimePlanningPlugin/Handlers/eFormCompletedHandler.cs
+++ ServiceTimePlanningPlugin/Handlers/eFormCompletedHandler.cs
@@ -23,7 +23,6 @@ SOFTWARE.
 */
 
 
-
 namespace ServiceTimePlanningPlugin.Handlers
 {
     using System;
@@ -32,10 +31,7 @@ namespace ServiceTimePlanningPlugin.Handlers
     using Infrastructure.Helpers;
     using Messages;
     using Microsoft.EntityFrameworkCore;
-    using Microting.eForm.Infrastructure;
     using Microting.eForm.Infrastructure.Constants;
-    using Microting.eForm.Infrastructure.Data.Entities;
-    using Microting.eForm.Infrastructure.Models;
     using Microting.TimePlanningBase.Infrastructure.Data;
     using Microting.TimePlanningBase.Infrastructure.Data.Entities;
     using Rebus.Handlers;
@@ -53,74 +49,102 @@ namespace ServiceTimePlanningPlugin.Handlers
 
         public async Task Handle(eFormCompleted message)
         {
-            var eformIdString = _dbContext.PluginConfigurationValues
-                .First(x => x.Name == "TimePlanningBaseSettings:EformId")
-                .Value;
-            var eformId = int.Parse(eformIdString);
-
-            await using var sdkDbContext = _sdkCore.DbContextHelper.GetDbContext();
-            var site = await sdkDbContext.Sites.SingleAsync(x => x.MicrotingUid == message.SiteId);
-            var cls = await sdkDbContext.Cases
-                .Where(x => x.SiteId == site.Id)
-                .Where(x => x.MicrotingUid == message.MicrotingId)
-                .OrderBy(x => x.DoneAt)
-                .LastOrDefaultAsync();
-            if (cls != null && cls.CheckListId == eformId)
+            Console.WriteLine($"EFormCompletedHandler .Handle called");
+            Console.WriteLine($"message.CheckId: {message.CheckId}");
+            Console.WriteLine($"message.MicrotingId: {message.MicrotingId}");
+            Console.WriteLine($"message.SiteId: {message.SiteId}");
+            Console.WriteLine($"message.CheckUId: {message.CheckUId}");
+            try
             {
-                var language = await sdkDbContext.Languages.SingleAsync(x => x.Id == site.LanguageId);
-                var fieldValues = await _sdkCore.Advanced_FieldValueReadList(new() { cls.Id }, language);
+                var eformIdString = _dbContext.PluginConfigurationValues
+                    .First(x => x.Name == "TimePlanningBaseSettings:EformId")
+                    .Value;
+                var eformId = int.Parse(eformIdString);
 
-                var dateValue = DateTime.Parse(fieldValues.First().Value);
-                var shift1Start = string.IsNullOrEmpty(fieldValues[1].Value) ? 0 : int.Parse(fieldValues[1].Value);
-                var shift1Pause = string.IsNullOrEmpty(fieldValues[2].Value) ? 0 : int.Parse(fieldValues[2].Value);
-                var shift1Stop = string.IsNullOrEmpty(fieldValues[3].Value) ? 0 : int.Parse(fieldValues[3].Value);
-                var shift2Start = string.IsNullOrEmpty(fieldValues[4].Value) ? 0 : int.Parse(fieldValues[4].Value);
-                var shift2Pause = string.IsNullOrEmpty(fieldValues[5].Value) ? 0 : int.Parse(fieldValues[5].Value);
-                var shift2Stop = string.IsNullOrEmpty(fieldValues[6].Value) ? 0 : int.Parse(fieldValues[6].Value);
-
-                var assignedSiteId = await _dbContext.AssignedSites
-                    .Where(x => x.SiteId == message.SiteId
-                                && x.WorkflowState != Constants.WorkflowStates.Removed)
-                    .Select(x => x.Id)
-                    .FirstOrDefaultAsync();
-
-                var timePlanning = await _dbContext.PlanRegistrations
-                    .Where(x => x.AssignedSiteId == assignedSiteId
-                                && x.Date == dateValue)
-                    .FirstOrDefaultAsync();
-
-                if (timePlanning == null)
+                await using var sdkDbContext = _sdkCore.DbContextHelper.GetDbContext();
+                var site = await sdkDbContext.Sites.SingleOrDefaultAsync(x => x.MicrotingUid == message.SiteId);
+                if (site == null)
                 {
-                    var newPlan = new PlanRegistration
-                    {
-                        AssignedSiteId = assignedSiteId,
-                        Date = dateValue,
-                        Pause1Id = shift1Pause,
-                        Pause2Id = shift2Pause,
-                        Start1Id = shift1Start,
-                        Start2Id = shift2Start,
-                        Stop1Id = shift1Stop,
-                        Stop2Id = shift2Stop,
-                    };
-
-                    await newPlan.Create(_dbContext);
+                    Console.WriteLine($"Site with MicrotingUid: {message.SiteId} not found");
+                    return;
                 }
-                else
+                var cls = await sdkDbContext.Cases
+                    .Where(x => x.SiteId == site.Id)
+                    .Where(x => x.MicrotingUid == message.MicrotingId)
+                    .OrderBy(x => x.DoneAt)
+                    .LastOrDefaultAsync();
+                if (cls != null && cls.CheckListId == eformId)
                 {
-                    if (timePlanning.Pause1Id == 0 || timePlanning.Pause2Id == 0
-                                                   || timePlanning.Start1Id == 0 || timePlanning.Start2Id == 0
-                                                   || timePlanning.Stop1Id == 0 || timePlanning.Stop2Id == 0)
+                    var language = await sdkDbContext.Languages.SingleOrDefaultAsync(x => x.Id == site.LanguageId);
+                    if (language == null)
                     {
-                        timePlanning.Pause1Id = timePlanning.Pause1Id == 0 ? shift1Pause : timePlanning.Pause1Id;
-                        timePlanning.Pause2Id = timePlanning.Pause2Id == 0 ? shift2Pause : timePlanning.Pause2Id;
-                        timePlanning.Start1Id = timePlanning.Start1Id == 0 ? shift1Start : timePlanning.Start1Id;
-                        timePlanning.Start2Id = timePlanning.Start2Id == 0 ? shift2Start : timePlanning.Start2Id;
-                        timePlanning.Stop1Id = timePlanning.Stop1Id == 0 ? shift1Stop : timePlanning.Stop1Id;
-                        timePlanning.Stop2Id = timePlanning.Stop2Id == 0 ? shift2Stop : timePlanning.Stop2Id;
+                        Console.WriteLine($"Language with Id: {site.LanguageId} not found");
+                        return;
+                    }
+                    var fieldValues = await _sdkCore.Advanced_FieldValueReadList(new() { cls.Id }, language);
 
-                        await timePlanning.Update(_dbContext);
+                    var dateValue = DateTime.Parse(fieldValues.First().Value);
+                    var shift1Start = string.IsNullOrEmpty(fieldValues[1].Value) ? 0 : int.Parse(fieldValues[1].Value);
+                    var shift1Pause = string.IsNullOrEmpty(fieldValues[2].Value) ? 0 : int.Parse(fieldValues[2].Value);
+                    var shift1Stop = string.IsNullOrEmpty(fieldValues[3].Value) ? 0 : int.Parse(fieldValues[3].Value);
+                    var shift2Start = string.IsNullOrEmpty(fieldValues[4].Value) ? 0 : int.Parse(fieldValues[4].Value);
+                    var shift2Pause = string.IsNullOrEmpty(fieldValues[5].Value) ? 0 : int.Parse(fieldValues[5].Value);
+                    var shift2Stop = string.IsNullOrEmpty(fieldValues[6].Value) ? 0 : int.Parse(fieldValues[6].Value);
+
+                    var assignedSiteId = await _dbContext.AssignedSites
+                        .Where(x => x.SiteId == message.SiteId
+                                    && x.WorkflowState != Constants.WorkflowStates.Removed)
+                        .Select(x => x.Id)
+                        .FirstOrDefaultAsync();
+                    if (assignedSiteId == 0)
+                    {
+                        Console.WriteLine($"AssignedSite with SiteId: {message.SiteId} not found");
+                        return;
+                    }
+
+                    var timePlanning = await _dbContext.PlanRegistrations
+                        .Where(x => x.AssignedSiteId == assignedSiteId
+                                    && x.Date == dateValue)
+                        .FirstOrDefaultAsync();
+
+                    if (timePlanning == null)
+                    {
+                        var newPlan = new PlanRegistration
+                        {
+                            AssignedSiteId = assignedSiteId,
+                            Date = dateValue,
+                            Pause1Id = shift1Pause,
+                            Pause2Id = shift2Pause,
+                            Start1Id = shift1Start,
+                            Start2Id = shift2Start,
+                            Stop1Id = shift1Stop,
+                            Stop2Id = shift2Stop,
+                        };
+
+                        await newPlan.Create(_dbContext);
+                    }
+                    else
+                    {
+                        if (timePlanning.Pause1Id == 0 || timePlanning.Pause2Id == 0
+                                                       || timePlanning.Start1Id == 0 || timePlanning.Start2Id == 0
+                                                       || timePlanning.Stop1Id == 0 || timePlanning.Stop2Id == 0)
+                        {
+                            timePlanning.Pause1Id = timePlanning.Pause1Id == 0 ? shift1Pause : timePlanning.Pause1Id;
+                            timePlanning.Pause2Id = timePlanning.Pause2Id == 0 ? shift2Pause : timePlanning.Pause2Id;
+                            timePlanning.Start1Id = timePlanning.Start1Id == 0 ? shift1Start : timePlanning.Start1Id;
+                            timePlanning.Start2Id = timePlanning.Start2Id == 0 ? shift2Start : timePlanning.Start2Id;
+                            timePlanning.Stop1Id = timePlanning.Stop1Id == 0 ? shift1Stop : timePlanning.Stop1Id;
+                            timePlanning.Stop2Id = timePlanning.Stop2Id == 0 ? shift2Stop : timePlanning.Stop2Id;
+
+                            await timePlanning.Update(_dbContext);
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
             }
         }
     }
