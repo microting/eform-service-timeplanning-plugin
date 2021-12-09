43d958b4d10263e1dd3a04f1bc9129dc8c40d49d
René Schultz Madsen <rm@microting.com>
2021-12-09 21:25:51 +0000

    Adding resources to service.

--- ServiceTimePlanningPlugin/Handlers/eFormCompletedHandler.cs
+++ ServiceTimePlanningPlugin/Handlers/eFormCompletedHandler.cs
@@ -23,6 +23,14 @@ SOFTWARE.
 */
 
 
+using System.Globalization;
+using System.Threading;
+using Microting.eForm.Dto;
+using Microting.eForm.Infrastructure.Constants;
+using Microting.eForm.Infrastructure.Data.Entities;
+using Microting.eForm.Infrastructure.Models;
+using ServiceTimePlanningPlugin.Resources;
+
 namespace ServiceTimePlanningPlugin.Handlers
 {
     using System;
@@ -182,7 +190,7 @@ namespace ServiceTimePlanningPlugin.Handlers
                     Message theMessage =
                         await _dbContext.Messages.SingleOrDefaultAsync(x => x.Id == timePlanning.MessageId);
                     string messageText = theMessage != null ? theMessage.Name : "";
-                    timePlanning.StatusCaseId = await timePlanning.DeployResults(maxHistoryDays, infoeFormId, _sdkCore, site, folderId, messageText);
+                    timePlanning.StatusCaseId = await DeployResults(timePlanning, maxHistoryDays, infoeFormId, _sdkCore, site, folderId, messageText);
                     await timePlanning.Update(_dbContext);
                     if (_dbContext.PlanRegistrations.Any(x => x.Date >timePlanning.Date && x.SdkSitId == site.MicrotingUid))
                     {
@@ -208,7 +216,7 @@ namespace ServiceTimePlanningPlugin.Handlers
                                     messageText = theMessage != null ? theMessage.EnName : "";
                                     break;
                             }
-                            planRegistration.StatusCaseId = await planRegistration.DeployResults(maxHistoryDays, infoeFormId, _sdkCore, site, folderId, messageText);
+                            planRegistration.StatusCaseId = await DeployResults(planRegistration, maxHistoryDays, infoeFormId, _sdkCore, site, folderId, messageText);
                             await planRegistration.Update(_dbContext);
                             preSumFlex = planRegistration.SumFlex;
                         }
@@ -221,6 +229,63 @@ namespace ServiceTimePlanningPlugin.Handlers
                 Console.WriteLine(ex.Message);
                 Console.WriteLine(ex.StackTrace);
             }
+        }
+
+        private async Task<int> DeployResults(PlanRegistration planRegistration, int maxHistoryDays, int eFormId, eFormCore.Core core, Site siteInfo, int folderId, string messageText)
+        {
+            if (planRegistration.StatusCaseId != 0)
+            {
+                    await core.CaseDelete(planRegistration.StatusCaseId);
+            }
+            await using var sdkDbContext = core.DbContextHelper.GetDbContext();
+            var language = await sdkDbContext.Languages.SingleAsync(x => x.Id == siteInfo.LanguageId);
+            var folder = await sdkDbContext.Folders.SingleOrDefaultAsync(x => x.Id == folderId);
+            var mainElement = await core.ReadeForm(eFormId, language);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(language.LanguageCode);
+            CultureInfo ci = new CultureInfo(language.LanguageCode);
+            mainElement.Label = planRegistration.Date.ToString("dddd dd. MMM yyyy", ci);
+            mainElement.EndDate = DateTime.UtcNow.AddDays(maxHistoryDays);
+            DateTime startDate = new DateTime(2020, 1, 1);
+            mainElement.DisplayOrder = (startDate - planRegistration.Date).Days;
+            DataElement element = (DataElement)mainElement.ElementList.First();
+            element.Label = mainElement.Label;
+            element.DoneButtonEnabled = false;
+            CDataValue cDataValue = new CDataValue
+            {
+                InderValue = $"<strong>{Translations.NettoHours}: {planRegistration.NettoHours:0.00}</strong><br/>" +
+                             $"{messageText}"
+            };
+            element.Description = cDataValue;
+            DataItem dataItem = element.DataItemList.First();
+            dataItem.Color = Constants.FieldColors.Yellow;
+            dataItem.Label = $"<strong>{Translations.Date}: {planRegistration.Date.ToString("dddd dd. MMM yyyy", ci)}</strong>";
+            cDataValue = new CDataValue
+            {
+                InderValue = $"{Translations.PlanText}: {planRegistration.PlanText}<br/>"+
+                             $"{Translations.PlanHours}: {planRegistration.PlanHours}<br/><br/>" +
+                             $"{Translations.Shift_1__start}: {planRegistration.Options[planRegistration.Start1Id > 0 ? planRegistration.Start1Id - 1 : 0]}<br/>" +
+                             $"{Translations.Shift_1__pause}: {planRegistration.Options[planRegistration.Pause1Id > 0 ? planRegistration.Pause1Id - 1 : 0]}<br/>" +
+                             $"{Translations.Shift_1__end}: {planRegistration.Options[planRegistration.Stop1Id > 0 ? planRegistration.Stop1Id - 1 : 0]}<br/><br/>" +
+                             $"{Translations.Shift_2__start}: {planRegistration.Options[planRegistration.Start2Id > 0 ? planRegistration.Start2Id - 1 : 0]}<br/>" +
+                             $"{Translations.Shift_2__pause}: {planRegistration.Options[planRegistration.Pause2Id > 0 ? planRegistration.Pause2Id - 1 : 0]}<br/>" +
+                             $"{Translations.Shift_2__end}: {planRegistration.Options[planRegistration.Stop2Id > 0 ? planRegistration.Stop2Id - 1 : 0]}<br/><br/>" +
+                             $"<strong>{Translations.NettoHours}: {planRegistration.NettoHours:0.00}</strong><br/><br/>" +
+                             $"{Translations.Flex}: {planRegistration.Flex:0.00}<br/>" +
+                             $"{Translations.SumFlex}: {planRegistration.SumFlex:0.00}<br/>" +
+                             $"{Translations.PaidOutFlex}: {planRegistration.PaiedOutFlex:0.00}<br/><br/>" +
+                             $"{Translations.Message}: {messageText}<br/><br/>"+
+                             $"<strong>{Translations.Comments}:</strong><br/>" +
+                             $"{planRegistration.WorkerComment}<br/><br/>" +
+                             $"<strong>{Translations.Comment_office}:</strong><br/>" +
+                             $"{planRegistration.CommentOffice}<br/><br/>" +
+                             $"<strong>{Translations.Comment_office_all}:</strong><br/>" +
+                             $"{planRegistration.CommentOffice}<br/>"
+            };
+            dataItem.Description = cDataValue;
+
+            if (folder != null) mainElement.CheckListFolderName = folder.MicrotingUid.ToString();
+
+            return (int)await core.CaseCreate(mainElement, "", (int)siteInfo.MicrotingUid, folderId);
         }
 
 
